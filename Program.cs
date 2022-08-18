2178c129aba67341ef53170c8de1e4bf02ada891
Andrii Banduliak <banduliak1303@gmail.com>
2022-08-18 22:16:15 +0300

    2nd task is done

--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
-﻿/* Задача 35: Задайте одномерный массив случайных чисел. Найдите количество элементов массива, 
-значения которых лежат в отрезке [10,99].
- */
-
+﻿/* **Задача 37:** Найдите произведение пар чисел в одномерном массиве. 
+Парой считаем первый и последний элемент, 
+второй и предпоследний и т.д. Результат запишите в новом массиве.
+[1 2 3 4 5] -> 5 8 3
+[6 7 3 6] -> 36 21 */
 
 using static System.Console;
 Clear();
 WriteLine("Введите число ");
 int numb = Convert.ToInt32(ReadLine());
 int[] mass = GetArray(numb);
-WriteLine($"{String.Join(" ",mass)}");
-WriteLine($"Количество элементов в отрезке {GetCountNumber(mass,10,99)}");
+WriteLine($"{String.Join(" ", mass)}");
+int[] massSum = GetSum(mass);
+WriteLine($"{String.Join(" ", massSum)}");
+
+
+
 
 
 
@@ -18,22 +23,26 @@ int[] GetArray(int L)
     int[] result = new int[L];
     for (int i = 0; i < result.Length; i++)
     {
-        result[i] = new Random().Next(0, 1000);
+        result[i] = new Random().Next(0, 10);
     }
 
     return result;
 }
 
-int GetCountNumber(int[] array, int minCount, int maxCount)
+int[] GetSum(int[] array)
 {
-    int count = 0;
-for (int i = 0; i < array.Length; i++)
+    int length = array.Length % 2 == 0 ? array.Length / 2 : array.Length / 2 + 1;
+    int[] ArraySum = new int[length];
+for (int i = 0; i < ArraySum.Length; i++)
 {
-    if(array[i]>=minCount && array[i]<=maxCount)
-    {
-        count+=1;
-    }
+   if (array[i]!=array[(array.Length-1)-i])
+   {
+     ArraySum[i]=array[i]*array[(array.Length-1)-i];
+   }
+   else
+   {
+    ArraySum[i]=array[i];
+   }
 }
-
-    return count;
+    return ArraySum;
 }
