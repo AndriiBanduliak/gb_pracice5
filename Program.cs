623800a76a93619075bc09fe7cd68653ab05d2bb
Andrii Banduliak <banduliak1303@gmail.com>
2022-08-18 21:08:38 +0300

    1st task is done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!");
+﻿/* **Задача 32:** Напишите программу замена элементов массива: 
+положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2] */
+using static System.Console;
+Clear();
+
+int[] array = GetArray(8);
+WriteLine($"{String.Join(" ", array)}");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] *= (-1);
+}
+WriteLine($"{String.Join(" ", array)}");
+
+
+
+
+int[] GetArray(int L)
+{
+    int[] result = new int[L];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(-9, 10);
+    }
+
+    return result;
+}
